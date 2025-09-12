5944438c0bae693878017f4c77632c38a149e588
seisochan <yuzukishiramine@gmail.com>
2025-09-12 23:06:00 +0700

    Debloating GNOME (as per nixOS wiki guide)

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -103,6 +103,13 @@
     ];
   };
 
+  # To disable installing GNOME's suite of applications
+  # and only be left with GNOME shell.
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
+
   # Install firefox.
   # programs.firefox.enable = true;
 
